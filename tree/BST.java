7624c444d5c3bbd6cccecbb364094ddc0c5be659
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-12-21 23:41:24 +0530

    added delete

--- tree/BST.java
+++ tree/BST.java
@@ -45,4 +45,38 @@ public class BST<T extends Comparable<T>> {
         else
             return true;
     }
+
+    public void delete(final T data) {
+        Root = delete(Root, data);
+    }
+
+    private Node<T> delete(final Node<T> node, final T val) {
+        if (node == null)
+            return null;
+        final int compare = val.compareTo(node.data);
+        if (compare < 0)
+            node.left = delete(node.left, val);
+        else if (compare > 0)
+            node.right = delete(node.right, val);
+        else {
+            if (node.left == null)
+                return node.right;
+            if (node.right == null)
+                return node.left;
+            T successor = min(node.right);
+            node.data = successor;
+            node.right = delete(node.right, successor);
+        }
+        return node;
+    }
+
+    public T min() {
+        return min(Root);
+    }
+
+    private T min(Node<T> node) {
+        while (node.left != null)
+            node = node.left;
+        return node.data;
+    }
 }
